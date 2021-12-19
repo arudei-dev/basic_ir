81a3a40f0e30c9a5520030a10a25910714f09fc1
Aldey Wahyu Putra <aldey.putra@tokopedia.com>
2021-12-19 08:08:41 +0700

    init Parser

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,4 @@
 pub mod constants;
 pub mod lexer;
+pub mod parser;
 pub mod token;
---
+++ src/core/parser/mod.rs
@@ -0,0 +1,6 @@
+// Grammar
+// Expr : Term ((+ | -) Term)*
+// Term : Factor ((* | /) Factor)*
+// Factor : Int | Float
+
+struct Parser {}
