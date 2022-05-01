feee79da80c285ebb8a3653a3fd1ca1d0995c008
Aldey Wahyu Putra <aldey.putra@tokopedia.com>
2022-05-01 12:48:04 +0700

    nuked all

--- src/core/constants/regexes.rs
+++
@@ -1,5 +0,0 @@
-use lazy_regex::{regex, Lazy, Regex};
-
-pub static DIGITS_DOT: &Lazy<Regex> = regex!("\\d|\\.");
-
-pub static SPACES_TABS: &Lazy<Regex> = regex!("\\s|\\t");
--- src/core/parser/mod.rs
+++
@@ -1,6 +0,0 @@
-// Grammar
-// Expr : Term ((+ | -) Term)*
-// Term : Factor ((* | /) Factor)*
-// Factor : Int | Float
-
-struct Parser {}
